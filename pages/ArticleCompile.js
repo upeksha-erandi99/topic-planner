9d66f1c9542c0d5dfc7bf6d1513edc9ea491026e
upeksha-erandi99 <upekshaerandi35@gmail.com>
2023-06-10 19:41:36 +0530

    m

---
+++ pages/ArticleCompile.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import MyTextEditor from './TextEditor'
+
+const ArticleCompile = () => {
+  return (
+    <div>
+        <MyTextEditor/>
+    </div>
+  )
+}
+
+export default ArticleCompile
